5b906a76cac8bcc5cbce33ab1cf5bdbf506927e1
sasawendler <101825787+sasawendler@users.noreply.github.com>
2024-09-19 12:06:59 +0000

    Pagina js

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let j = 0; j < botoes.length; j++){
+    botoes[j].onclick = function(){
+        
+    }
+}
